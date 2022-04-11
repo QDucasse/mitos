786ab19ebaba6fa03ee70aeee34fc08d74c81da8
Quentin DUCASSE <quentin.ducasse@ensta-bretagne.org>
2022-04-11 18:00:21 +0200

    base node with accept meta-definition

--- src/mitos/ast.py
+++ src/mitos/ast.py
@@ -9,90 +9,97 @@ Created on Sun Aug 18 20:35:45 2019
 def empty_if_none(value):
     return value if value is not None else []
 
+class BaseNode:
+    def accept(self, visitor):
+        """Meta-defining the accept method for the subclasses"""
+        # Formatting the name
+        class_name_camel_case = self.__class__.__name__
+        class_name_snake_case = re.sub(r'(?<!^)(?=[A-Z])', '_', class_name_camel_case).lower()
+        method_name = getattr(visitor, "visit_" + class_name_snake_case)
+        # Casting the visit<class_name> method on the visitor
+        method_name(visitor)
 
-class GrammarNode:
+
+class GrammarNode(BaseNode):
     """grammar = { rule };"""
     def __init__(self, rules=None):
         self.rules = empty_if_none(rules)
 
-    def accept(self, visitor):
-        self.visitor.visit_grammar(self)
 
-
-class RuleNode:
+class RuleNode(BaseNode):
     """rule = identifier , "=" , expression , ";" ;"""
     def __init__(self, identifier=None, definitions=None):
         self.identifier = identifier
         self.definitions = empty_if_none(definitions)
 
 
-class DefinitionNode:
+class DefinitionNode(BaseNode):
     """definition = term, {',', term};"""
     def __init__(self, terms=None):
         self.terms = empty_if_none(terms)
 
 
-class TermNode:
+class TermNode(BaseNode):
     """term = factor, ['-', exception];"""
     def __init__(self, factor=None, exception=None):
         self.factor = factor
         self.exception = exception
 
 
-class FactorNode:
+class FactorNode(BaseNode):
     """factor = [integer, '*'], primary;"""
     def __init(self, integer=None, primary=None):
         self.integer = integer
         self.primary = primary
 
 
-class PrimaryNode:
+class PrimaryNode(BaseNode):
     """primary = option | repetition | group | special | string | identifier | empty;"""
     def __init__(self, expression=None):
         self.expression = expression
 
 
-class OptionNode:
+class OptionNode(BaseNode):
     """option = '[',  definition, {'|', definition}, ']';"""
     def __init__(self, definitions=None):
         self.definitions = empty_if_none(definitions)
 
 
-class RepetitionNode:
+class RepetitionNode(BaseNode):
     """repetition = '{',  definition, {'|', definition}, '}';"""
     def __init__(self, definitions=None):
         self.definitions = empty_if_none(definitions)
 
 
-class GroupNode:
+class GroupNode(BaseNode):
     """group = '(',  definition, {'|', definition}, ')';"""
     def __init__(self, definitions=None):
         self.definitions = empty_if_none(definitions)
 
 
-class EmptyNode:
+class EmptyNode(BaseNode):
     """empty = ;"""
 
 
-class SpecialNode:
+class SpecialNode(BaseNode):
     """special = REGEXED!"""
     def __init__(self, sequence=None):
         self.sequence = sequence
 
 
-class IdentifierNode:
+class IdentifierNode(BaseNode):
     """identifier = REGEXED!"""
     def __init__(self, identifier=None):
         self.identifier = identifier
 
 
-class IntegerNode:
+class IntegerNode(BaseNode):
     """integer = REGEXED!"""
     def __init__(self, integer=None):
         self.integer = integer
 
 
-class StringNode:
+class StringNode(BaseNode):
     """string = REGEXED!"""
     def __init__(self, string=None):
         self.string = string
