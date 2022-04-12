d3d6caf1cd25fdd8c6e2a4b05e4eb3392162f0f3
Quentin DUCASSE <quentin.ducasse@ensta-bretagne.org>
2022-04-12 11:38:56 +0200

    Simple visitor

--- src/mitos/visitor.py
+++ src/mitos/visitor.py
@@ -5,110 +5,70 @@ Created on Sun Aug 18 20:35:45 2019
 @author: Quentin Ducasse & Kevin Bedin
 """
 
-class Visitor(object):
-    '''
-    Main visitor class
-    Templating the successive accept calls in order to be used by
-    the subclasses lexerWriter and parserWriter
-    '''
-    def visit(self,grammar):
-        grammar.accept(self,grammar)
+class Visitor:
+    """Placeholder example of a visitor, pretty print"""
 
-    def visitGrammar(self,grammar):
-        '''Visits grammar'''
-        # Visit the syntax element
-        syntax = grammar.syntax
-        syntax.accept(self,syntax)
+    def visit(self, node):
+        node.accept(self)
 
-    def visitSyntax(self,syntax):
-        '''Visits all syntax rules'''
-        for syntaxRule in syntax.syntaxRules:
-            syntaxRule.accept(self,syntaxRule)
+    def visit_grammar(self, grammar):
+        """grammar = { rule };"""
+        for rule in grammar.rules:
+            self.visit(rule)
 
-    def visitSyntaxRule(self,syntaxRule):
-        '''Visits one syntax rule'''
-        # Visit identifier
-        id   = syntaxRule.identifier
-        # Visit all the definitions
-        defs = syntaxRule.definitions
-        id.accept(self,id)
-        defs.accept(self,defs)
+    def visit_rule(self, rule):
+        """rule  = identifier, '=', definition, {'|', definition} ';';"""
+        self.visit(rule.identifier)
+        for definition in rule.definitions:
+            self.visit(definition)
 
-    def visitDefinitions(self,definitions):
-        '''Visits all definitions'''
-        for definition in definitions.definitions:
-            definition.accept(self,definition)
-
-    def visitDefinition(self,definition):
-        '''Visit a definition'''
-        # Visits all terms
+    def visit_definition(self, definition):
+        """definition = term, {',', term};"""
         for term in definition.terms:
-            term.accept(self,term)
+            self.visit(term)
 
-    def visitTerm(self,term):
-        '''Visit a term'''
-        # Visits the factor
-        term.factor.accept(self,term.factor)
-        # Visits the exception
-        exception = term.exception
-        if exception != None:
-            term.exception.accept(self,term.exception)
+    def visit_term(self, term):
+        """term = factor, ['-', factor];"""
+        self.visit(term.factor)
+        if term.exception is not None:
+            self.visit(term.exception)
 
-    def visitException(self,exception):
-        '''Visit the exception'''
-        exception.factor.accept(self,exception.factor)
+    def visit_factor(self, factor):
+        """factor = [integer, '*'], primary;"""
+        if factor.integer is not None:
+            self.visit(factor.integer)
+        self.visit(factor.primary)
 
-    def visitFactor(self,factor):
-        '''Visit a factor'''
-        # Visits the primary
-        factor.primary.accept(self,factor.primary)
+    def visit_primary(self, primary):
+        """primary = option | repetition | group | special | string | identifier | empty;"""
+        self.visit(primary.expression)
 
-    def visitPrimary(self,primary):
-        '''Visit a primary'''
-        if primary.optionalSeq != None:
-            primary.optionalSeq.accept(self,primary.optionalSeq)
-        elif primary.identifier != None:
-            primary.identifier.accept(self,primary.identifier)
-        elif primary.repeatedSeq != None:
-            primary.repeatedSeq.accept(self,primary.repeatedSeq)
-        elif primary.groupedSeq != None:
-            primary.groupedSeq.accept(self,primary.groupedSeq)
-        elif primary.specialSeq != None:
-            primary.specialSeq.accept(self,primary.specialSeq)
-        elif primary.terminalString != None:
-            primary.terminalString.accept(self,primary.terminalString)
-        elif primary.empty != None:
-            primary.empty.accept(self,primary.empty)
+    def visit_option(self, option):
+        """option = '[',  definition, {'|', definition}, ']';"""
+        for definition in option.definitions:
+            self.visit(definition)
 
-    def visitOptionalSeq(self,optionalSeq):
-        '''Visit an optional sequence'''
-        optionalSeq.definitions.accept(self,optionalSeq.definitions)
+    def visit_repetition(self, repetition):
+        """repetition = '{',  definition, {'|', definition}, '}';"""
+        for definition in repetition.definitions:
+            self.visit(definition)
 
-    def visitRepeatedSeq(self,repeatedSeq):
-        '''Visit a repeated sequence'''
-        repeatedSeq.definitions.accept(self,repeatedSeq.definitions)
+    def visit_group(self, group):
+        """group = '(',  definition, {'|', definition}, ')';"""
+        for definition in group.definitions:
+            self.visit(definition)
 
-    def visitGroupedSeq(self,groupedSeq):
-        '''Visit a grouped sequence'''
-        groupedSeq.definitions.accept(self,groupedSeq.definitions)
+    def visit_empty(self, empty):
+        """empty = ;"""
 
-    def visitSpecialSeq(self,specialSeq):
-        pass
+    def visit_special(self, special):
+        """special = REGEXED!"""
 
-    def visitTerminalString(self,terminalString):
-        pass
+    def visit_identifier(self, identifier):
+        """identifier = REGEXED!"""
 
-    def visitTerminalStringSQuote(self,terminalStringSQuote):
-        pass
+    def visit_integer(self, integer):
+        """integer = REGEXED!"""
 
-    def visitTerminalStringDQuote(self,terminalStringDQuote):
-        pass
-
-    def visitIdentifier(self,identifier):
-        pass
-
-    def visitEmpty(self,empty):
-        pass
-
-    def visitInteger(self,integer):
-        pass
+    def visit_string(self, string):
+        """string = REGEXED!"""
